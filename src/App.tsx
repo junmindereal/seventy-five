a07f0cf0786e9dd59ecb5b86124d55164d36e792
junmindereal <junmindereal@gmail.com>
2020-04-21 17:10:28 +0800

    seventy-five init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
+  const styles = {
+    container: 'container xl mx-auto',
+    logo: 'w-16',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <img className={styles.logo} src={logo} alt="Seventy Five Logo" />
     </div>
   );
 }
