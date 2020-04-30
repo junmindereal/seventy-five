8dbab9e93bf528387b1e0178b610236016bc7452
Junmin De Real <junmin.dereal@ewave.com>
2020-04-30 18:38:32 +0800

    Modal on progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,11 @@ module.exports = {
         '1000': '#610404',
       },
     },
+    inset: {
+      '0': 0,
+      '1/10': '10%',
+      '1/20': '5%',
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'group-hover', 'even'],
