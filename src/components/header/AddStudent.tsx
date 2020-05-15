d44fd3771494ca55521f8a756b8deb9c4671d573
Junmin De Real <junmindereal@gmail.com>
2020-05-15 09:50:08 +0800

    Refactor add modal component

--- src/components/header/AddStudent.tsx
+++ src/components/header/AddStudent.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Modal from 'react-modal';
 import {btn} from '../../styles/btn';
-import {modal} from '../../styles/modal';
-import {form} from '../../styles/form';
 import {Icon} from '@iconify/react';
 import userPlus from '@iconify/icons-uil/user-plus';
-import multiplyIcon from '@iconify/icons-uil/multiply';
+import AddModal from '../modals/AddModal';
 
 const AddStudent: React.SFC = () => {
   const [modalIsOpen, setIsOpen] = React.useState(false);
@@ -24,80 +21,7 @@ const AddStudent: React.SFC = () => {
         <Icon className={btn.secondaryIcon} icon={userPlus}></Icon>
         <span className={btn.secondaryText}>Add Student</span>
       </button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        shouldCloseOnOverlayClick={true}
-        closeTimeoutMS={150}
-        className={modal.modal}
-      >
-        <div className={modal.container}>
-          <header className={modal.header}>
-            <h2 className={modal.heading}>Add Student</h2>
-            <button className={modal.close} onClick={closeModal}>
-              <Icon icon={multiplyIcon} />
-            </button>
-          </header>
-          <form>
-            <div className={form.group}>
-              <label className={form.label} htmlFor="firstName">
-                First Name
-              </label>
-              <input
-                className={form.input}
-                type="text"
-                id="firstName"
-                placeholder="Juan"
-              />
-            </div>
-            <div className={form.group}>
-              <label className={form.label} htmlFor="middleName">
-                Middle Name
-              </label>
-              <input
-                className={form.input}
-                type="text"
-                id="middleName"
-                placeholder="Mercado"
-              />
-            </div>
-            <div className={form.group}>
-              <label className={form.label} htmlFor="lastName">
-                Last Name
-              </label>
-              <input
-                className={form.input}
-                type="text"
-                id="lastName"
-                placeholder="Dela Cruz"
-              />
-            </div>
-            <div className={form.group}>
-              <label className={form.label} htmlFor="section">
-                Section
-              </label>
-              <input
-                className={form.input}
-                type="text"
-                id="sectionk"
-                placeholder="3 - Wisdom"
-              />
-            </div>
-            <div className={form.action}>
-              <button
-                className={btn.tertiary}
-                onClick={closeModal}
-                type="button"
-              >
-                Cancel
-              </button>
-              <button className={`${btn.primary} ${form.submit}`} type="submit">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </Modal>
+      <AddModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
     </React.Fragment>
   );
 };
---
+++ src/components/modals/AddModal.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Modal from 'react-modal';
+import {btn} from '../../styles/btn';
+import {modal} from '../../styles/modal';
+import {form} from '../../styles/form';
+import {Icon} from '@iconify/react';
+import multiplyIcon from '@iconify/icons-uil/multiply';
+
+interface AddModalProps {
+  modalIsOpen: boolean;
+  closeModal: () => void;
+}
+
+const AddModal: React.FC<AddModalProps> = ({modalIsOpen, closeModal}) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      shouldCloseOnOverlayClick={true}
+      closeTimeoutMS={150}
+      className={modal.modal}
+    >
+      <div className={modal.container}>
+        <header className={modal.header}>
+          <h2 className={modal.heading}>Add Student</h2>
+          <button className={modal.close} onClick={closeModal}>
+            <Icon icon={multiplyIcon} />
+          </button>
+        </header>
+        <form>
+          <div className={form.group}>
+            <label className={form.label} htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              className={form.input}
+              type="text"
+              id="firstName"
+              placeholder="Juan"
+            />
+          </div>
+          <div className={form.group}>
+            <label className={form.label} htmlFor="middleName">
+              Middle Name
+            </label>
+            <input
+              className={form.input}
+              type="text"
+              id="middleName"
+              placeholder="Mercado"
+            />
+          </div>
+          <div className={form.group}>
+            <label className={form.label} htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              className={form.input}
+              type="text"
+              id="lastName"
+              placeholder="Dela Cruz"
+            />
+          </div>
+          <div className={form.group}>
+            <label className={form.label} htmlFor="section">
+              Section
+            </label>
+            <input
+              className={form.input}
+              type="text"
+              id="sectionk"
+              placeholder="3 - Wisdom"
+            />
+          </div>
+          <div className={form.action}>
+            <button className={btn.tertiary} onClick={closeModal} type="button">
+              Cancel
+            </button>
+            <button className={`${btn.primary} ${form.submit}`} type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default AddModal;
